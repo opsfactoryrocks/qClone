dd658429fbab228663c4f351253feaefa26f5e34
Michael Crilly <mc@sent.com>
2017-04-11 17:31:26 +1000

    sorting out cli options and responses

--- main.go
+++ main.go
@@ -62,7 +62,7 @@ func getAllUserRepositories() ([]Repository, error) {
 	return repositories, nil
 }
 
-func isBundle(name string) bool {return true}
+func isBundle(name string) bool { return false }
 
 func isUserRepository(name string, repositories []Repository) bool {
 	for _, r := range repositories {
@@ -74,31 +74,70 @@ func isUserRepository(name string, repositories []Repository) bool {
 	return false
 }
 
+func usage() {
+	fmt.Println("./qclone command [clone-to-path]")
+}
+func version() {
+	fmt.Println("qClone Version " + QCLONE_VERSION)
+}
+
+func fetchCache() ([]Repository, error)           {}
+func cacheRepositories(repositories []Repository) {}
+
 func parseCommand() {
+	var repositories []Repository
 	arguments := os.Args[1:]
+
+	if len(arguments) == 0 {
+		// We expect at least one aurgment
+		usage()
+		return
+	}
+	if arguments[0] == "version" {
+		// We want to avoid fetching a repository
+		// list for a non-repo related command
+		version()
+		return
+	}
+
+	// Now fetch repositories as commands beyond
+	// this point will need them
+	// TODO: cache this somewhere
 	repositories, err := getAllUserRepositories()
 	checkErrorAndPanic(err)
+
 	switch len(arguments) {
-	case 0:
-		for i, r := range repositories {
-			fmt.Printf("[%d] %s\n", i+1, r.Name)
-		}
 	case 1:
-		fmt.Println("Clone repository")
+		if arguments[0] == "list" {
+			for i, r := range repositories {
+				fmt.Printf("[%d] %s\n", i+1, r.Name)
+			}
+		}
+		// Look for a bundle first
 		if isBundle(arguments[0]) {
-			fmt.Println("I would clone a bundle: %s\n", arguments[0])
+			if len(arguments) > 1 && arguments[1] != "" {
+				fmt.Printf("I would clone bundle %s to %s\n", arguments[0], arguments[1])
+			} else {
+				fmt.Printf("I would clone bundle %s to %s\n", arguments[0], QCLONE_DEFAULT_HOME)
+			}
+			return
 		}
+
+		// Now look for a repository
 		if isUserRepository(arguments[0], repositories) {
-			fmt.Printf("I would clone: %s\n", arguments[0])
+			if len(arguments) > 1 && arguments[1] != "" {
+				fmt.Printf("I would clone %s to %s\n", arguments[0], arguments[1])
+			} else {
+				fmt.Printf("I would clone %s to %s\n", arguments[0], QCLONE_DEFAULT_HOME)
+			}
+			return
 		}
-	case 2:
-		fmt.Println("Clone a bundle")
-		if isBundle(arguments[0]) {
-			fmt.Println("I would clone a bundle '%s' to '%s'\n", arguments[0], arguments[1])
-		}
-		if isUserRepository(arguments[0], repositories) {
-			fmt.Printf("I would clone: '%s' to '%s'\n", arguments[0], arguments[1])
-		}
+
+		// TODO: look for a third-party user's repo
+		// TODO: look for user/repo-name
+
+		// Nothing to do at this point
+		fmt.Println("Not sure what do at this point. Exiting")
 	}
 }
 
